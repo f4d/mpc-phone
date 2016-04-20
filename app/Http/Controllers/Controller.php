564517c321cc1f7e934bfcf9bfa859557cac0312
David Powers <david@1xrun.com>
2016-04-20 13:44:03 -0500

    pulling digits in from http GET

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,10 +18,12 @@ class Controller extends BaseController
   public function moo(Request $request)
 	{
 		//?Digits=9342390942
-	  $msg_id = $request->input('Digits');
+		//?id=9342390942
+	  $msg_id = $_GET[íd];
+	  //$msg_id = $request->input('Digits');
 	  //$msg_id = '9342390942';
 	  $url = 'http://petguardian.staging.wpengine.com/wp-json/petguardian/v1/ivr-notification?lookup=';
-	  get_headers();
+	  get_headers($url.$msg_id);
 		return $url.$msg_id;  	
 		/*
 	  $response = new Services_Twilio_Twiml;
